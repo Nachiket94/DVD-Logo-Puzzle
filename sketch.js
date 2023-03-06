0e46a6a18a419a0912c5026c269cc5e923f5e32a
Mr._CG_04 <83511180+Nachiket94@users.noreply.github.com>
2023-03-06 12:59:09 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,32 +1,33 @@
 let xspeed;
 let yspeed;
 let x, y;
-
+let img;
 function setup() {
-  //createCanvas(400, 400);
+  img = loadImage('dvd-logo.jpg');
+  createCanvas(600, 600);
   x = 100;
   y = 100;
   xspeed = 4;
   yspeed = 3;
-  //background(0);
 }
 
 function draw() {
-  createCanvas(400, 400);
-  background(255);
-  fill(200, 0, 150, 175);
-  stroke(0, 0, 255);
-  strokeWeight(3);
-  rect(x, y, 50, 50);
+  background(0);
+  frameRate(50);
+  // fill(200, 0, 150, 175);
+  // stroke(0, 0, 255);
+  // strokeWeight(2);
+  // rect(x, y, 50, 50)
+  image(img, x, y, 100 , 60);
 
   x = x + xspeed;
   y = y + yspeed;
 
-  if (x + 50 > width || x == 0) {
+  if (x + 100 > width || x == 0) {
     xspeed = xspeed * -1;
   }
 
-  if (y + 50 > height || y < 0) {
+  if (y + 60 > height || y < 0) {
     yspeed = yspeed * -1;
   }
 
